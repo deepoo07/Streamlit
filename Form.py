a2073ef51e0a1ffcf2869905043682a4398f4889
Deepanti Poonekar <deepoonekar07@gmail.com>
2025-07-13 15:44:57 +0530

    Add files via upload

--- Form.py
+++ Form.py
@@ -1,14 +1,26 @@
 import streamlit as st
+st.info('This is Data Science form.',icon="📊")
 with st.form("form"):
     st.write("Please fill the form")
-    name=st.text_input("Name:")
-    college_name=st.text_input("Institution Name:")
-    Percentage=st.number_input("Percentage:",min_value=0.0, max_value=100.0, format="%.2f")
-    Working=st.radio("Are you currently working?", ("Yes", "No"))
+    name=st.text_input("*Name:")
+    surname=st.text_input("Surname:")
+    options = ["MSU Baroda", "IIT Bombay", "Other"]
+    selected = st.selectbox("Institution:", options)
+    if selected == "Other":
+        college_name = st.text_input("*Enter your institution name:")
+    else:
+        college_name = selected
+
+    Percentage=st.number_input("*Percentage:",min_value=0.0, max_value=100.0, format="%.2f")
+    Working=st.radio("*Are you currently working?", ("Yes", "No"))
     
     submitted=st.form_submit_button("Submit")
     if submitted:
-        st.write("Name:",name)
-        st.write("Institution Name:",college_name)
-        st.write(f"Percentage: {Percentage:.2f}%")
-        st.write("Job Status:", Working)
+        if not name or not college_name or not Percentage or not Working:
+            st.warning("⚠️ Please fill in all the * required fields.")
+        else:
+            st.success("Form submitted successfully!", icon="✅")
+            st.write("Name:",name, surname)
+            st.write("Institution Name:",college_name)
+            st.write(f"Percentage: {Percentage:.2f}%")
+            st.write("Job Status:", Working)
